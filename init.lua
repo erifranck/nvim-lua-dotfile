4a69690d45f5362b4633f8b2d8b8c02ed51a38ac
erifranck <erifranjose@hotmail.com>
2023-03-21 14:18:28 -0500

    remove unused plugins

--- init.lua
+++ init.lua
@@ -2,14 +2,13 @@ require "env"
 require 'plugins'
 
 -- Coc configuration completion and snippets
---require "coc-setting"
+-- require "coc-setting"
 
 require "nvim-cmp"
 -- require "ide-setting"
 require "glance-settings"
 require "aerial-settings"
---
-require 'blankline-config'
+-- require 'blankline-config'
 require 'starline-config'
 require 'lualine-config'
 require 'mason-settings'
